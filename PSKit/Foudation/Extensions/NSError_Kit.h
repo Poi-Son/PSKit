25427bb7ef87c8b71685ec32909104c37f16210e
PoiSon <git@yerl.cn>
2016-03-04 16:29:08 +0800

    新增PSAssertError断言，抛出NSError

--- PSKit/Foudation/Extensions/NSError_Kit.h
+++ PSKit/Foudation/Extensions/NSError_Kit.h
@@ -9,8 +9,24 @@
 #import <Foundation/Foundation.h>
 #import <PSKit/PSKitDefines.h>
 
+#define PSAssertError(condition, desc, ...) \
+    do { \
+        __PRAGMA_PUSH_NO_EXTRA_ARG_WARNINGS \
+        if (!(condition)) { \
+            @throw [NSError ps_errorWhenExecuting: __func__ \
+                                           inFile: [NSString stringWithUTF8String:__FILE__] ?: @"<Unknown File>" \
+                                           atLine: __LINE__ \
+                                      description: desc, ##__VA_ARGS__]; \
+        } \
+        __PRAGMA_POP_NO_EXTRA_ARG_WARNINGS \
+    } while(0)
+
+NS_ASSUME_NONNULL_BEGIN
+
 PSKIT_EXTERN NSError *NSErrorWithLocalizedDescription(NSString *description);
 
 @interface NSError (PSKit)
 + (instancetype)ps_errorWithLocalizedDescription:(NSString *)description;
++ (instancetype)ps_errorWhenExecuting:(const char *)function inFile:(NSString *)fileName atLine:(NSInteger)line description:(nullable NSString *)format, ...;
 @end
+NS_ASSUME_NONNULL_END
--- PSKit/Foudation/Extensions/NSError_Kit.m
+++ PSKit/Foudation/Extensions/NSError_Kit.m
@@ -8,6 +8,7 @@
 
 #import "NSError_Kit.h"
 #import "NSBundle_Kit.h"
+#import "PSFoudation.h"
 
 @implementation NSError (PSKit)
 + (instancetype)ps_errorWithLocalizedDescription:(NSString *)description{
@@ -18,6 +19,25 @@
         domain = domain ?: @"none domain";
     });
     return [self errorWithDomain:domain code:-1000 userInfo:@{NSLocalizedDescriptionKey: description}];
+}
+
++ (instancetype)ps_errorWhenExecuting:(const char *)function inFile:(NSString *)fileName atLine:(NSInteger)line description:(NSString *)format, ...{
+    static NSString *domain;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        domain = [NSBundle mainBundle].infoDictionary[PSBundleName];
+        domain = domain ?: @"none domain";
+    });
+    
+    va_args(format);
+    NSString *reason = [[NSString alloc] initWithFormat:format arguments:format_args];
+    
+    return [self errorWithDomain:domain code:-1000 userInfo:@{
+                                                              NSLocalizedDescriptionKey: reason,
+                                                              @"Function": @(function),
+                                                              @"Line": @(line),
+                                                              @"FileName": [fileName lastPathComponent]
+                                                              }];
 }
 @end
 
